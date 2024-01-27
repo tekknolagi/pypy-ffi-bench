2c3788172e569bd2c08f5612b926197916656cdb
Max Bernstein <max@bernsteinbear.com>
2024-01-27 00:17:27 -0500

    Use macros

--- signature.c
+++ signature.c
@@ -38,45 +38,33 @@ PyObject* add(PyObject* module, PyObject*const *args, Py_ssize_t nargs) {
 }
 
 #ifdef METH_TYPED
-int inc_arg_types[] = {T_C_LONG, -1};
+#define LIST(...) __VA_ARGS__
 
-PyPyTypedMethodMetadata inc_sig = {
-  .arg_types = inc_arg_types,
-  .ret_type = T_C_LONG,
-  .underlying_func = inc_impl,
-  .ml_name = "inc",
-};
+#define SIG(name, args, ret) \
+  int name##_arg_types[] = {args, -1};\
+  PyPyTypedMethodMetadata name##_sig = {\
+    .arg_types = name##_arg_types,\
+    .ret_type = ret,\
+    .underlying_func = name##_impl,\
+    .ml_name = #name,\
+  };
 
-int wrong_arg_types[] = {100, -1};
-
-PyPyTypedMethodMetadata wrong_sig = {
-  .arg_types = wrong_arg_types,
-  .ret_type = T_C_LONG,
-  .underlying_func = inc_impl,
-  .ml_name = "wrong",
-};
-
-int add_arg_types[] = {T_C_DOUBLE, T_C_DOUBLE, -1};
-
-PyPyTypedMethodMetadata add_sig = {
-  .arg_types = add_arg_types,
-  .ret_type = T_C_DOUBLE,
-  .underlying_func = add_impl,
-  .ml_name = "add",
-};
-
-static PyMethodDef signature_methods[] = {
-    {inc_sig.ml_name, inc, METH_O | METH_TYPED, "Add one to an int"},
-    {wrong_sig.ml_name, inc, METH_O | METH_TYPED, "Have a silly signature"},
-    {add_sig.ml_name, (PyCFunction)(void*)add, METH_FASTCALL | METH_TYPED, "Add two doubles"},
-    {NULL, NULL, 0, NULL}};
+#define TYPED_SIG(name, fptr, type, doc) \
+  {name##_sig.ml_name, (PyCFunction)(void*)fptr, type | METH_TYPED, doc}
 #else
-static PyMethodDef signature_methods[] = {
-    {"inc", inc, METH_O, "Add one to an int"},
-    {"wrong", inc, METH_O, "Have a silly signature"},
-    {"add", (PyCFunction)(void*)add, METH_FASTCALL, "Add two doubles"},
-    {NULL, NULL, 0, NULL}};
+#define SIG(...)
+#define TYPED_SIG(name, fptr, type, doc) \
+  {#name, (PyCFunction)(void*)fptr, type, doc}
 #endif
+
+SIG(inc, LIST(T_C_LONG), T_C_LONG)
+SIG(add, LIST(T_C_DOUBLE, T_C_DOUBLE), T_C_DOUBLE)
+
+static PyMethodDef signature_methods[] = {
+  TYPED_SIG(inc, inc, METH_O, "Add one to an int"),
+  TYPED_SIG(add, add, METH_FASTCALL, "Add two doubles"),
+  {NULL, NULL, 0, NULL},
+};
 
 static struct PyModuleDef signature_definition = {
     PyModuleDef_HEAD_INIT, "signature",
