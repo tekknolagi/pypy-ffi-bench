9196e2ad97dcc813ec9668236abbce3d7ce4b2be
Max Bernstein <max@bernsteinbear.com>
2024-02-08 13:43:46 -0500

    Exclude graal23 and use taskset and 3 runs

--- manual/benchmark.sh
+++ manual/benchmark.sh
@@ -4,7 +4,7 @@ set -eux
 
 hyperfine --export-json results.json \
   -L runtime \
-  ~/Documents/dev/pypy-git/pypy/goal/pypy3.10-c,pypy3.8,python3.8,python3.10,python3.11,python3.12,python3.13,~/.pyenv/versions/graalpython-22.2.0/bin/graalpython,~/.pyenv/versions/graalpy-community-23.1.0/bin/python \
+  ~/Documents/dev/pypy-git/pypy/goal/pypy3.10-c,pypy3.8,python3.8,python3.10,python3.11,python3.12,python3.13,~/.pyenv/versions/graalpython-22.2.0/bin/graalpython \
   --setup '{runtime} setup.py build_ext --inplace' \
-  --warmup 1 --runs 10 \
-  '{runtime} ffibench.py'
+  --runs 3 \
+  'taskset -c 0 {runtime} ffibench.py'
